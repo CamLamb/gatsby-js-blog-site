2a250c3bdd71b080d45c380b629bceeab48bef23
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2017-10-07 16:57:04 +0100

    setup gatsby

---
+++ .cache/async-requires.js
@@ -0,0 +1,26 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/cameronlamb/shared/gatsby-js/.cache/dev-404-page.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/cameronlamb/shared/gatsby-js/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/cameronlamb/shared/gatsby-js/src/pages/index.js"),
+  "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/cameronlamb/shared/gatsby-js/src/pages/page-2.js")
+}
+
+exports.json = {
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/cameronlamb/shared/gatsby-js/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/cameronlamb/shared/gatsby-js/.cache/json/404.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/cameronlamb/shared/gatsby-js/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/cameronlamb/shared/gatsby-js/.cache/json/page-2.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/cameronlamb/shared/gatsby-js/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": require("gatsby-module-loader?name=component---src-layouts-index-js!/Users/cameronlamb/shared/gatsby-js/.cache/layouts/index.js")
+}
---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/cameronlamb/shared/gatsby-js/src/layouts/index.js"
+  import data from "/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
