090a991b45104b3a86433aa10df7a5053b0b49ca
Cameron Lamb <jafacakes2011@hotmail.co.uk>
2017-10-08 00:59:03 +0100

    cleanup?

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -3,17 +3,17 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-article-js": require("gatsby-module-loader?name=component---src-templates-article-js!/Users/cameronlamb/shared/gatsby-js/src/templates/article.js"),
-  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/cameronlamb/shared/gatsby-js/src/pages/404.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/cameronlamb/shared/gatsby-js/src/pages/index.js")
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/cameronlamb/shared/gatsby-js/src/pages/index.js"),
+  "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/cameronlamb/shared/gatsby-js/src/pages/404.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "first-post.json": require("gatsby-module-loader?name=path---first-post!/Users/cameronlamb/shared/gatsby-js/.cache/json/first-post.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
-  "404.json": require("gatsby-module-loader?name=path---404!/Users/cameronlamb/shared/gatsby-js/.cache/json/404.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/cameronlamb/shared/gatsby-js/.cache/json/index.json"),
+  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/cameronlamb/shared/gatsby-js/.cache/json/404.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "404-html.json": require("gatsby-module-loader?name=path---404-html!/Users/cameronlamb/shared/gatsby-js/.cache/json/404-html.json")
 }
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,17 +4,17 @@ const preferDefault = m => m && m.default || m
 
 exports.components = {
   "component---src-templates-article-js": preferDefault(require("/Users/cameronlamb/shared/gatsby-js/src/templates/article.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/cameronlamb/shared/gatsby-js/src/pages/404.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/cameronlamb/shared/gatsby-js/src/pages/index.js"))
+  "component---src-pages-index-js": preferDefault(require("/Users/cameronlamb/shared/gatsby-js/src/pages/index.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/cameronlamb/shared/gatsby-js/src/pages/404.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "first-post.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/first-post.json"),
   "layout-index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
-  "404.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/404.json"),
-  "layout-index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/index.json"),
+  "layout-index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
+  "404.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/404.json"),
   "layout-index.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/layout-index.json"),
   "404-html.json": require("/Users/cameronlamb/shared/gatsby-js/.cache/json/404-html.json")
 }
